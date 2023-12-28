c22fad265a22a77d56b7c14365f321b8edc735b6
chaba7 <chaba7@FRATELLO.local>
2023-12-28 21:09:22 +0100

    add IP info

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,25 @@
 import os
 import sys
+import socket
+
+def get_public_ip():
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.connect(("8.8.8.8", 80))
+		public_ip = sock.getsockname()[0]
+		return str(public_ip)
+	except:
+		pass
+	return ("0.0.0.0")
+
+def get_local_ip():
+	return ("0.0.0.0")
+	try:
+		local_ip = socket.gethostbyname(socket.gethostname())
+		return str(local_ip)
+	except:
+		pass
+	return ("0.0.0.0")
 
 def getlistdir(dirname, callback = None):
 	files = []
@@ -27,6 +47,8 @@ def get_stable_diffusion_webui_dir():
 	return (None)
 
 if (__name__ == "__main__"):
+	print ("YOUR PUBLIC IP: " + str(get_public_ip()))
+	print ("YOUR LOCAL IP : " + str(get_local_ip()))
 	args = " ".join(sys.argv[1:])
 	cmd = (
 '''
