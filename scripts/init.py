fccc79c329897c43f2834c55de5c598fc1a36a01
chaba7 <chaba7@FRATELLO.local>
2024-01-01 20:49:51 +0100

    update init.py

--- scripts/init.py
+++ scripts/init.py
@@ -3,7 +3,8 @@ ST_REPO_URL     = "https://github.com/AUTOMATIC1111/stable-diffusion-webui.git"
 CVT_REPO_URL    = "https://github.com/Abdelmathin/civitai-models-downloader.git"
 ROOT_DIR        = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 ROOT_PARENT_DIR = os.path.dirname(ROOT_DIR)
-
+ST_DIR          = ROOT_PARENT_DIR + "/stable-diffusion-webui"
+CVT_DIR         = ROOT_PARENT_DIR + "/civitai-models-downloader"
 commands = [
 	"sudo apt-get update",
 	"sudo apt-get install pip",
@@ -11,12 +12,14 @@ commands = [
 	'''cd "''' + ROOT_PARENT_DIR + '''" && git clone "''' + CVT_REPO_URL + '''"'''
 ]
 
+models = {
+	"Stable-diffusion/sd_xl_base_1.0.safetensors" : "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors?download=true"
+}
+
 if (__name__ == "__main__"):
 	for cmd in commands:
 		os.system (cmd)
-
-
-
-
-
-
+	for model_path, model_url in models.items():
+		model_file = ST_DIR + "/models/" + model_path
+		if  os.path.exists(model_file):
+			print ('cd "' + CVT_DIR + '" && python3 script.py "' + model_url + '" "' + model_file + '"')
