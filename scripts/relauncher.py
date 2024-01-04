53ffb21c63c36e1a5aead776727d8ff78fc2fe0e
chaba7 <chaba7@FRATELLO.local>
2024-01-04 19:15:24 +0100

    update relauncher.py

--- scripts/relauncher.py
+++ scripts/relauncher.py
@@ -4,19 +4,15 @@ ROOT_PARENT_DIR = os.path.dirname(ROOT_DIR)
 
 cmd = (
 '''
-echo "Starting your application..."
-cd  {0}/bed-story-front-end
-make dev &
 
-echo "Starting watcher..."
-cd {0}/stable-diffusion-webui-initializer/outputs/
-python3 -m http.server 3003 &
+echo "Init stable-diffusion-webui..." && cd "{0}/stable-diffusion-webui-initializer/" && make init
 
-echo "Starting stable-diffusion-webui..."
-cd {0}/stable-diffusion-webui-initializer/
-make init
-make webui &
-echo "donne"
+echo "Starting your application..." && cd  {0}/bed-story-front-end && make dev &
+
+echo "Starting watcher..." && cd {0}/stable-diffusion-webui-initializer/outputs/ && python3 -m http.server 3003 &
+
+echo "Starting stable-diffusion-webui..." && cd {0}/stable-diffusion-webui-initializer/ && make webui &
+
 '''.format(ROOT_PARENT_DIR).strip())
 
 os.system(cmd)
