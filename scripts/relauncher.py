35ea066bc068ffd823a83a208d39572fca03c32d
chaba7 <chaba7@FRATELLO.local>
2024-01-04 19:36:52 +0100

    update relauncher.py

--- scripts/relauncher.py
+++ scripts/relauncher.py
@@ -1,7 +1,9 @@
 import os
+import time
+
 WORKSPACE_DIR = "/workspace"
 
-cmd = (
+launch_string = (
 '''
 
 echo "Init stable-diffusion-webui..." && cd "''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/" && make init
@@ -11,8 +13,24 @@ echo "Starting your application..." && cd  ''' + WORKSPACE_DIR + '''/bed-story-f
 mkdir -p ''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/outputs/
 echo "Starting watcher..." && cd ''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/outputs/ && python3 -m http.server 3003 &
 
-echo "Starting stable-diffusion-webui..." && cd ''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/ && make webui &
+echo "Starting stable-diffusion-webui..." && cd ''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/ && make webui
 
 '''.strip())
 
-os.system(cmd)
+def relaunch_process(launch_counter=0):
+	while True:
+		print("- -" * 20)
+		print("Relauncher: Launching...")
+		if launch_counter > 0:
+			print("\tRelaunch count: " + str(launch_counter))
+		try:
+			os.system(launch_string)
+		except Exception as err:
+			print("An error occurred: ", err)
+		finally:
+			print(">>>>>>>>>>>>>>>>>>>>>> Relauncher: Process is ending. Relaunching in 2s... <<<<<<<<<<<<<<<<<<<<<")
+			launch_counter += 1
+			time.sleep(2)
+
+if __name__ == "__main__":
+    relaunch_process()
