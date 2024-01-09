37e2b3e73dff10a87be822a4ece3bb559eb6aeb2
chaba7 <chaba7@FRATELLO.local>
2024-01-09 14:50:49 +0100

    rename app

--- scripts/relauncher.py
+++ scripts/relauncher.py
@@ -12,7 +12,7 @@ rm -rf /stable-diffusion-webui
 
 echo "Init stable-diffusion-webui..." && cd "''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/" && make init
 
-echo "Starting your application..." && cd  ''' + WORKSPACE_DIR + '''/bed-story-front-end && make dev &
+echo "Starting your application..." && cd  ''' + WORKSPACE_DIR + '''/app && make &
 
 mkdir -p ''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/outputs/
 echo "Starting watcher..." && cd ''' + WORKSPACE_DIR + '''/stable-diffusion-webui-initializer/outputs/ && python3 -m http.server 3003 &
